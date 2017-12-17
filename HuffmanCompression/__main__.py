fe40b4d088c3e67dd5cd70c23dbb6c48fe4e3b91
alyswidan <swidan12340@gmail.com>
2017-12-17 02:00:57 +0200

    cli implementation

--- HuffmanCompression/__main__.py
+++ HuffmanCompression/__main__.py
@@ -1,6 +1,15 @@
 from HuffmanCompression.helpers import *
+import sys
 
+if len(sys.argv) != 3:
+    print('illegal usage: use as HuffmanCompression [compress|decompress] filename')
+    exit(1)
+if sys.argv[1] == 'compress':
+    compression_ratio, code_dict = compress(sys.argv[2])
+    print('compression ratio = ', compression_ratio)
+    print('codes = ', code_dict)
 
-cr,d = compress('test.test')
-decompress('test.test')
-print(cr)
+elif sys.argv[1] == 'decompress':
+    decompress(sys.argv[2])
+else:
+    print('illegal option ',sys.argv[2])
