b26b02f2bb867bbe043c807a0027d5218203a95b
alyswidan <swidan12340@gmail.com>
2017-12-19 11:26:31 +0200

    compressing a binary file

--- HuffmanCompression/__main__.py
+++ HuffmanCompression/__main__.py
@@ -1,7 +1,6 @@
 from HuffmanCompression.helpers import *
 import sys
 import time
-
 if len(sys.argv) != 3:
     print('illegal usage: use as HuffmanCompression [compress|decompress] filename')
     exit(1)
@@ -15,5 +14,15 @@ if sys.argv[1] == 'compress':
 
 elif sys.argv[1] == 'decompress':
     decompress(sys.argv[2])
+elif sys.argv[1] == 'all':
+    compression_ratio, code_dict = compress(sys.argv[2])
+    print('done compressing')
+    print(len(code_dict.keys()))
+    header_dict = decompress(sys.argv[2]+'.comp')
+    print('done decompressing')
+    print(len(header_dict.keys()))
+    for i in code_dict.keys():
+        if i not in header_dict.keys():
+            print('codes[{0}] = {1} not found'.format(i,code_dict[i]))
 else:
-    print('illegal option ',sys.argv[2])
+    print('illegal option ', sys.argv[2])
